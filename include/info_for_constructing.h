d3eefad015b6fd869fb62789ab8cb1dbfa55c69a
gvs1977 <vladimir.s.gavrilov@gmail.com>
2018-01-08 11:47:01 +0300

    I began to reimplement the struct of the constructing info.

---
+++ include/info_for_constructing.h
@@ -0,0 +1,39 @@
+/*
+    File:    info_for_constructing.h
+    Created: 08 January 2018 at 11:18 Moscow time
+    Author:  Гаврилов Владимир Сергеевич
+    E-mails: vladimir.s.gavrilov@gmail.com
+             gavrilov.vladimir.s@mail.ru
+             gavvs1977@yandex.ru
+*/
+
+#ifndef INFO_FOR_CONSTRUCTING_H
+#define INFO_FOR_CONSTRUCTING_H
+#include <string>
+#include <utility>
+#include <cstddef>
+#include <cstdint>
+#include <vector>
+#include "../include/command.h"
+
+namespace info_for_constructing{
+    struct Regexps{
+        Command_buffer numbers;
+        Command_buffer strings;
+        Command_buffer idents;
+    };
+
+    using Lexem_with_code = std::pair<std::string, size_t>;
+    using Lexems          = std::vector<Lexem_with_code>;
+
+    struct Codes_info{
+    };
+
+    struct Info{
+        Regexps     regexps;
+        Codes_info  codes_info;
+        std::string name_of_scaner_class;
+        std::string scaner_file_name_without_ext;
+    };
+};
+#endif
